eacfe4ee8e14d046f54acd9e9f0073cbe9462a59
Dan Stroud <dstroud@mac.com>
2023-12-03 16:53:28 -0600

    v0.1

--- README.md
+++ README.md
@@ -1,22 +1,23 @@
 # Combiner
-Mod for Norns to turn 2x128 or 2x64 Grids into 1 virtual Grid
+Mod for Norns to turn 2x128 or 2x64 Grids into one virtual Grid
 
 >"Over the course of their endless wars, the Cybertronian race has developed a frightful array of weapons, but the most powerful and versatile invention of all might be the power to physically merge multiple Cybertronians into a single form—this power might be unlocked through conventional Cybertronian science, or through the energies of a mystical artifact like the Enigma of Combination. The term combiner (sometimes capitalized, sometimes not) can be used to refer to both those rare Transformers who possess this ability and the composite machine they create"
 
 [Transformers Wiki](https://tfwiki.net/wiki/Combiner)
 
 ### What it is
-A Norns mod that takes 2 Grids and combines them to form 1 larger virtual Grid available to scripts.
+A Norns mod that takes two Grids and combines them to form one larger virtual Grid available to any script that supports that size.
 
 ### Requirements
-2x64 Grids or 2x128 Grids. Probably works with clones but I haven't tested.
+- 2x64 Grids or 2x128 Grids. Let me know if it works with clones?
+- Norns 231114
 
 ### How to use it
 1. Install from the Maiden project manager (or `;install https://github.com/dstroud/combiner`)
-2. Add your Grids as ports *3 and 4* in SYSTEM>>DEVICES>>GRID.
+2. Add like-sized Grids to **ports 3 and 4** in SYSTEM>>DEVICES>>GRID.
 3. Enable the mod in SYSTEM>>MODS>>E3 (+ symbol) and restart.
-4. Edit mod settings via SYSTEM>>MODS>>COMBINER>>K3.
-5. *"Virtual grid 128/256"* will appear in appear to scripts as port 1 (default for `grid.connect()`).
+4. Edit mod settings via SYSTEM>>MODS>>COMBINER>>K3. Grid 'a' is port 3 and 'b' is port 4. LED intensity may not be supported on all devices.
+5. The virtual Grid will appear in appear to scripts on port 1 (default for most scripts).
 
 ### Notes
-If this doesn't work with your a script, it's probably because that script is using `grid.devices` rather than `grid.vports`. Changing this should be a quick fix for script authors but if there's a reason that's not possible, let me know.
+If this doesn't work with your a particular script, it's probably because that script is using `grid.devices` rather than `grid.vports`. Changing this should be a quick fix for script authors but if that's not possible for some reason, just let me know and I can probably find a workaround.
--- lib/mod.lua
+++ lib/mod.lua
@@ -1,114 +1,235 @@
 -- combiner: grid aggregator
+-- https://github.com/dstroud/combiner
+
 local mod = require 'core/mods'
 local filepath = "/home/we/dust/data/combiner/"
-local v = 1
-local a = 3
-local b = 4
+local combiner = {}
+combiner.menu = 1
+combiner.rows = 8
+combiner.rotation_a = 0
+combiner.rotation_b = 0
+combiner.intensity_a = 15
+combiner.intensity_b = 15
+local grid_v = grid.connect(1)  -- Virtual Grid
+local grid_a = grid.connect(3)  -- Grid 'a' hardcoded to vport 3
+local grid_b = grid.connect(4)  -- Grid 'b' hardcoded to vport 4
+local rot = {3, 2, 1, 0, 3, 2, 1}
+
+
+local function read_prefs()
+  prefs = {}
+  if util.file_exists(filepath.."prefs.data") then
+    prefs = tab.load(filepath.."prefs.data")
+    print('table >> read: ' .. filepath.."prefs.data")
+    combiner.rotation_a = prefs.rotation_a
+    combiner.rotation_b = prefs.rotation_b
+    combiner.intensity_a = prefs.intensity_a
+    combiner.intensity_b = prefs.intensity_b
+    grid_a:rotation(combiner.rotation_a)
+    grid_b:rotation(combiner.rotation_b)
+    grid_a:intensity(combiner.intensity_a)
+    grid_b:intensity(combiner.intensity_b)
+  end
+end
+
+
+local function write_prefs(from)
+  local prefs = {}
+  if util.file_exists(filepath) == false then
+    util.make_dir(filepath)
+  end
+  prefs.rotation_a = combiner.rotation_a
+  prefs.rotation_b = combiner.rotation_b
+  prefs.intensity_a = combiner.intensity_a
+  prefs.intensity_b = combiner.intensity_b
+  tab.save(prefs, filepath .. "prefs.data")
+  print("table >> write: " .. filepath.."prefs.data")
+end
 
 
 local function init_virtual()
-  local vport_path = grid.vports[v]
+  if grid_a.cols == 16 and grid_b.cols == 16 then -- technically allows a Zero ;)
+    print("Combiner: Configuring virtual 16x16 Grid")
+    combiner.rows = 16
+  elseif grid_a.cols == 8 and grid_b.cols == 8 then
+    print("Combiner: Configuring virtual 16x8 Grid")
+    combiner.rows = 8
+  else
+    print("Combiner: Add like-sized Grids in SYSTEM>>DEVICES>>GRID ports 3 and 4")
+  end
 
-  grid.vports[v].name = "virtual 256"
-  grid.vports[v].rows = 16
-  grid.vports[v].cols = 16
-  
-  grid.vports[v].device = {
+  grid_v.name = "virtual 16x".. combiner.rows
+  grid_v.rows = combiner.rows
+  grid_v.cols = 16
+  grid_v.device = {
     id = 1,
     port = 1,
-    name = "virtual 256",
+    name = "virtual 16x".. combiner.rows,
     serial = "00000000",
     -- dev = NA, @tparam userdata dev : opaque pointer to device.
     cols = 16,
-    rows = 16,
+    rows = combiner.rows,
   }
 
-  function vport_path:led(x, y, val)
-    if y <= 8 then
-      grid_a:led(x, y, val)
-    else
-      grid_b:led(x, y - 8, val)
+  if combiner.rows == 16 then
+    function grid_v:led(x, y, val)
+      if y <= 8 then
+        grid_a:led(x, y, val)
+      else
+        grid_b:led(x, y - 8, val)
+      end
+    end
+  elseif combiner.rows == 8 then
+    function grid_v:led(x, y, val)
+      if x <= 8 then
+        grid_a:led(x, y, val)
+      else
+        grid_b:led(x - 8, y, val)
+      end
     end
   end
-  
-  function vport_path:all(val)
+
+  function grid_v:all(val)
     grid_a:all(val)
     grid_b:all(val)
   end
-  
-  function vport_path:refresh()
-    grid_a:refresh(val)
-    grid_b:refresh(val)
-  end  
+
+  function grid_v:refresh()
+    grid_a:refresh()
+    grid_b:refresh()
+  end
+
+  function grid_v:rotation()
+    -- supported through mod menu... is there a reason to pass through script rotation?
+  end
+
+  function grid_v:tilt_enable()
+    -- LOL
+  end
 
 end
 
 
 -- define new key input handlers that pass to virtual grid
 local function define_handlers()
-  
-  grid.vports[a].key = function(x, y, s)
-    if y <=8 then  -- little debug assist for Zero
+
+  if combiner.rows == 16 then
+    grid_a.key = function(x, y, s)
       grid_v.key(x, y, s)
     end
-  end
-  
-  grid.vports[b].key = function(x, y, s)
-    if y <=8 then  -- little debug assist for Zero
+
+    grid_b.key = function(x, y, s)
       local y = y + 8
       grid_v.key(x, y, s)
     end
+
+  elseif combiner.rows == 8 then
+    grid_a.key = function(x, y, s)
+      grid_v.key(x, y, s)
+    end
+
+    grid_b.key = function(x, y, s)
+      local x = x + 8
+      grid_v.key(x, y, s)
+    end
   end
-  
-end  
 
-
-local function connect_vports()
-  grid_v = grid.connect(1)  -- virtual grid
-  grid_a = grid.connect(a)  -- physical grid a
-  grid_b = grid.connect(b)  -- physical grid b
 end
-  
+
+
 mod.hook.register("system_post_startup", "combiner post startup", function()
-  
-  -- grid.vport tables are overwritten after system_post_startup hook with contents of grid.devices
-  -- can't just set grid.devices, however, because we don't have a dev to point to
-  -- script hooks happen after some scripts check for grid
-  -- weird solution: redefine grid.update_devices() to create a virtual grid
+  -- due to update_devices() overwriting vports after mod hook, redefine it
+  local old_update_devices = grid.update_devices
   function grid.update_devices()
-    grid.list = {}
-    for _,device in pairs(grid.devices) do
-      device.port = nil
-    end
-  
-    -- connect available devices to vports
-    for i=1,4 do
-      grid.vports[i].device = nil
-      grid.vports[i].rows = 0
-      grid.vports[i].cols = 0       
-  
-      for _,device in pairs(grid.devices) do
-        if device.name == grid.vports[i].name then
-          grid.vports[i].device = device
-          grid.vports[i].rows = device.rows
-          grid.vports[i].cols = device.cols
-          device.port = i
-        end
-      end
-    end
-    
-    -- our bits --
-    init_virtual()
-    connect_vports()
-
+    old_update_devices()
+    init_virtual()    -- generate virtual interface
+    read_prefs()      -- load rotation and intensity
   end
-
 end)
 
 
 -- requires norns 231114
-mod.hook.register("script_post_init", "combiner post init", function() 
+mod.hook.register("script_post_init", "combiner post init", function()
   define_handlers()
 end)
 
-  
+
+-- system mod menu for settings
+local m = {}
+
+function m.key(n, z)
+  if z == 1 then
+    if n == 2 then
+      mod.menu.exit()
+    end
+  end
+end
+
+
+function m.enc(n, d)
+  local d = util.clamp(d, -1, 1)
+    if n == 2 then
+    combiner.menu = util.clamp(combiner.menu + d, 1, 4)
+  elseif n == 3 then
+    if combiner.menu == 1 then
+      combiner.rotation_a = util.clamp(combiner.rotation_a - d, -3, 3)
+      grid_a:rotation(combiner.rotation_a)
+    elseif combiner.menu == 2 then
+        combiner.rotation_b = util.clamp(combiner.rotation_b - d, -3, 3)
+        grid_b:rotation(combiner.rotation_b)
+    elseif combiner.menu == 3 then
+      combiner.intensity_a = util.clamp(combiner.intensity_a + d, 0, 15)
+      grid_a:intensity(combiner.intensity_a)
+    elseif combiner.menu == 4 then
+      combiner.intensity_b = util.clamp(combiner.intensity_b + d, 0, 15)
+      grid_b:intensity(combiner.intensity_b)
+    end
+  end
+  m.redraw()
+end
+
+
+function m.redraw()
+  screen.clear()
+  screen.level(4)
+  screen.move(0, 10)
+  screen.text("MODS / COMBINER")
+
+  screen.move(0, 30)
+  screen.level(combiner.menu == 1 and 15 or 4)
+  screen.text("rotation a")
+  screen.move(127, 30)
+  screen.text_right(combiner.rotation_a * 90 .. "°")
+
+  screen.move(0, 40)
+  screen.level(combiner.menu == 2 and 15 or 4)
+  screen.text("rotation b")
+  screen.move(127, 40)
+  screen.text_right(combiner.rotation_b * 90 .. "°")
+
+  screen.move(0, 50)
+  screen.level(combiner.menu == 3 and 15 or 4)
+  screen.text("intensity a")
+  screen.move(127, 50)
+  screen.text_right(combiner.intensity_a)
+
+  screen.move(0, 60)
+  screen.level(combiner.menu == 4 and 15 or 4)
+  screen.text("intensity b")
+  screen.move(127, 60)
+  screen.text_right(combiner.intensity_b)
+
+  screen.update()
+end
+
+
+function m.init() -- on menu entry
+end
+
+
+function m.deinit() -- on menu exit
+  write_prefs()
+end
+
+
+mod.menu.register(mod.this_name, m)
